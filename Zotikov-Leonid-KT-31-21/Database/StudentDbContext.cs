83baaf3b51974aaa9694b2d2c769bed47b1f14be
Leonid-Zotikov-KT-31-21 <leonid-zotikov@mail.ru>
2024-10-16 23:19:01 +0300

    add dbcontext and CreateDatabase migration

---
+++ Zotikov-Leonid-KT-31-21/Database/StudentDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Zotikov_Leonid_KT_31_21.Database.Configurations;
+using Zotikov_Leonid_KT_31_21.Models;
+using Microsoft.EntityFrameworkCore;
+using IvanIvanovKt_31_20.Database.Configurations;
+
+
+namespace Zotikov_Leonid_KT_31_21.Database
+{
+    public class StudentDbContext : DbContext
+    {
+        //Добавляем таблицы
+        DbSet<Student> Students { get; set; }
+        DbSet<Group> Groups { get; set; }
+        DbSet<Subject> Subjects { get; set; }
+        DbSet<Zachet> Zachets { get; set; }
+        DbSet<Grade> Grades { get; set; }
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Добавляем конфигурации к таблицам
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new GroupConfiguration());
+            modelBuilder.ApplyConfiguration(new ZachetConfiguration());
+            modelBuilder.ApplyConfiguration(new GradeConfiguration());
+            modelBuilder.ApplyConfiguration(new SubjectConfiguration());
+           
+        }
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+    }
+}
